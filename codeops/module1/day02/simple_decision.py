6cc17854258d778fa441728d962e621942f0cbc4
Bons <bons6710hos@gmail.com>
2026-07-14 17:24:30 +0300

    Add simple decision-making script to evaluate pass or fail based on user score

---
+++ codeops/module1/day02/simple_decision.py
@@ -0,0 +1,9 @@
+
+#  simple decison make if user acore is >= 50 pass or fail
+score = int(input("Enter your score: "))
+
+if score >= 50:
+    print("Congratulations! You have passed.")
+else:
+    print("Sorry, you have failed. Better luck next time.")
+
