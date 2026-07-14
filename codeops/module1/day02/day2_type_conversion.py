00368f384f3dd1fe7b608a59bce9c81f8c9d50ab
Bons <bons6710hos@gmail.com>
2026-07-14 17:21:10 +0300

    Add  basic age calculator by accepting birth year

---
+++ codeops/module1/day02/day2_type_conversion.py
@@ -0,0 +1,9 @@
+
+#  type conversion and age calculation
+
+birth_year = input("Enter your birth year: ")
+birth_year = int(birth_year)
+current_year = 2026
+
+age = current_year - birth_year
+print(f"You are {age} years old.")
