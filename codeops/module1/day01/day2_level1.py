3417a48acf71dd8f6ceea1eb606f74b2fce578ef
Bons <bons6710hos@gmail.com>
2026-07-14 17:04:04 +0300

    Add initial implementation of personal information script for day 2 level 1

---
+++ codeops/module1/day01/day2_level1.py
@@ -0,0 +1,12 @@
+full_name="Bonsa Dealegn"
+age=25
+# in cm
+height=175 
+is_student=True
+if is_student==True:
+    is_student="Yes"
+else:
+    is_student="No"
+favorite_food="tibs"
+
+print(f"My name is {full_name}, I am {age} years old, my height is {height} cm, I am a student: {is_student}, and my favorite food is {favorite_food}.")
