610f39182a58fe111d0f5c67485b5c4eb96a833a
Bons <bons6710hos@gmail.com>
2026-07-14 17:40:55 +0300

    Add simple number pattern script to print numbers, odd numbers, and multiples of 5

---
+++ codeops/module1/day02/level2/number_pattern.py
@@ -0,0 +1,15 @@
+# simple number pattern using for loop
+for i in range(1, 20):
+    print(i )
+
+    # print odd numbers
+print("Odd numbers from 1 to 20:")
+for i in range(1, 20):
+    if i % 2 != 0:
+        print(i)
+print("only divisible by 5:")
+for i in range(1, 20):
+    if i % 5 == 0:
+        print(i)
+
+
