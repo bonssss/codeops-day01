d9314166206f40a02cfb7deb333f780e76583159
Bons <bons6710hos@gmail.com>
2026-07-16 09:12:21 +0300

    Add example demonstrating set creation and duplicate removal

---
+++ codeops/module1/day03/level1/sets.py
@@ -0,0 +1,9 @@
+# 3. Sets 
+# • Create a list with duplicate names. 
+# • Convert it to a set to remove duplicates. 
+# • Add a new name to the set.
+lst =[1,2,3,4,4,5,5,5]
+lst_to_set = set(lst)
+print(lst_to_set)
+lst_to_set.add(6)
+print(lst_to_set)
