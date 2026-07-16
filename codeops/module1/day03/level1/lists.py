feee909743158d5a74935eff7fe255e62e19b984
Bons <bons6710hos@gmail.com>
2026-07-16 09:06:38 +0300

    Add favorite cities list and Ethiopian coordinates tuple with print statements

---
+++ codeops/module1/day03/level1/lists.py
@@ -0,0 +1,18 @@
+favorite_city= ["Addis Ababa", "New York", "Tokyo", "Paris", "Sydney"]
+print(favorite_city)
+print(favorite_city[0])
+print(favorite_city[-1])
+# add new city
+favorite_city.append("London")
+print(favorite_city)
+# remove city
+favorite_city.remove("Tokyo")
+print(favorite_city)
+
+# create tuple of ethiopian coordinate
+ethiopian_coordinate = (9.145, 48.489673)
+print(ethiopian_coordinate)
+
+lat,lng=ethiopian_coordinate
+print(lat)
+print(lng)
