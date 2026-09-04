003451beee9b0a7b5b5c3c6b34e555b787eb39dd
Bons <bons6710hos@gmail.com>
2026-09-04 21:26:59 +0300

    feat: update document.title with current dish count on list change

--- codeops/module3/day_01/exercises/day1/src/components/Menu.jsx
+++ codeops/module3/day_01/exercises/day1/src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Card from './Card'
 import Dish from './Dish'
@@ -19,6 +19,11 @@ function Menu({ dishes }) {
     selectedCategory === "All"
       ? dishes
       : dishes.filter((dish) => dish.category === selectedCategory)
+
+  // Exercise 1: Update document.title to show the number of dishes currently shown
+  useEffect(() => {
+    document.title = `${filteredDishes.length} dishes shown`
+  }, [filteredDishes.length])
 
   return (
     <div className="menu-container">
