ff2b0c73c0b03b21e2b92ca1026f849549761d67
Bons <bons6710hos@gmail.com>
2026-09-07 21:46:29 +0300

    feat(ex5): add menu/:id route with useParams and link dish cards to detail page

--- codeops/module3/day_01/exercises/day1/src/App.jsx
+++ codeops/module3/day_01/exercises/day1/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Menu from './components/Menu'
+import DishDetail from './pages/DishDetail'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
               <Route path="menu" element={<Menu />} />
+              <Route path="menu/:id" element={<DishDetail />} />
               <Route path="about" element={<About />} />
               <Route path="contact" element={<Contact />} />
               <Route path="*" element={<NotFound />} />
